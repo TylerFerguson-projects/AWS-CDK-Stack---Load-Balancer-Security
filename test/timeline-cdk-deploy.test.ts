b11a6658d5dcf5990fc8584b6bafec81c921bee9
creek whistle <creekwhistle@gmail.com>
2025-02-24 11:49:48 -0600

    Initial commit

---
+++ test/timeline-cdk-deploy.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TimelineCdkDeploy from '../lib/timeline-cdk-deploy-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/timeline-cdk-deploy-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TimelineCdkDeploy.TimelineCdkDeployStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
