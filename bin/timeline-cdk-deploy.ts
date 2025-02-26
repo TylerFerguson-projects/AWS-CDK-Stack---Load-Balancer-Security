af49965354bb6a547473972efab8b355a9eb381d
Tyler Ferguson <tyler.ferguson@ibm.com>
2025-02-25 22:38:52 -0600

    bastion server security improvement

--- bin/timeline-cdk-deploy.ts
+++ bin/timeline-cdk-deploy.ts
@@ -1,10 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
+import * as dotenv from 'dotenv';
 import { TimelineCdkDeployStack } from '../lib/timeline-cdk-deploy-stack';
 
+dotenv.config();
+
 const app = new cdk.App();
+
+// Set context values from environment variables
+if (process.env.ALLOWED_IP_CIDR) {
+  app.node.setContext('ALLOWED_IP_CIDR', process.env.ALLOWED_IP_CIDR);
+}
+
 new TimelineCdkDeployStack(app, 'TimelineCdkDeployStack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
+ 
+  // CLI credentials
+   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
 });
